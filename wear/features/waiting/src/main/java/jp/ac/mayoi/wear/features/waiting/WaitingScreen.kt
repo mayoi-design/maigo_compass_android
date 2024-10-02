ad97901c1fd2b02bb89ef461844b2256bc9b2c24
ryoka-1104 <b1022160@fun.ac.jp>
2024-10-02 15:15:23 +0900

    待機画面のUIのひな型を作成

--- wear/features/waiting/src/main/java/jp/ac/mayoi/wear/features/waiting/WaitingScreen.kt
+++ wear/features/waiting/src/main/java/jp/ac/mayoi/wear/features/waiting/WaitingScreen.kt
@@ -1,2 +1,34 @@
 package jp.ac.mayoi.wear.features.waiting
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Text
+import androidx.wear.tooling.preview.devices.WearDevices
+
+
+@Composable
+fun Greeting(greetingName: String) {
+    Text(
+        modifier = Modifier.padding(),
+
+        text = "ようこそ"
+    )
+}
+@Preview(device = WearDevices.SMALL_ROUND, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    Text(
+        modifier = Modifier.padding(
+           start = 65.dp,
+            top = 86.dp,
+           ),
+        text = "ようこそ",
+
+        color = MaterialTheme.colors.primary,
+    )
+
+}
