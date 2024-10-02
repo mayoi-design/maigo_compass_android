8eddd92ff63727a054385f3d1fa627ee188863a0
Yourein <rein.you.3.1@gmail.com>
2024-10-02 12:38:23 +0900

    :phone:core:resourceに:common:resourceでの色定義を利用した定数を宣言

--- phone/core/resource/src/main/java/jp/ac/mayoi/core/resource/Colors.kt
+++ phone/core/resource/src/main/java/jp/ac/mayoi/core/resource/Colors.kt
@@ -1,2 +1,22 @@
 package jp.ac.mayoi.core.resource
 
+import androidx.compose.runtime.Composable
+import jp.ac.mayoi.common.resource.buttonColorsDefaultCommon
+import jp.ac.mayoi.common.resource.colorAccentCommon
+import jp.ac.mayoi.common.resource.colorAccentSecondaryCommon
+import jp.ac.mayoi.common.resource.colorBackgroundPrimaryLightCommon
+import jp.ac.mayoi.common.resource.colorBackgroundSecondaryLightCommon
+import jp.ac.mayoi.common.resource.colorTextCaptionLightCommon
+import jp.ac.mayoi.common.resource.colorTextMainLightCommon
+
+val colorBackgroundPrimary = colorBackgroundPrimaryLightCommon
+val colorBackgroundSecondary = colorBackgroundSecondaryLightCommon
+val colorTextMain = colorTextMainLightCommon
+val colorTextCaption = colorTextCaptionLightCommon
+val colorAccent = colorAccentCommon
+val colorAccentSecondary = colorAccentSecondaryCommon
+
+// ButtonColors
+
+val buttonColorsDefault
+    @Composable get() = buttonColorsDefaultCommon
