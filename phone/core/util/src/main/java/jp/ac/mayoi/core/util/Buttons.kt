1f62efc4d5ec97999c3aacbdc879850ccc0db5b5
“mahochi” <523.maho@gmail.com>
2024-10-04 17:04:24 +0900

    maigobuttonの周りに影を付けた

--- phone/core/util/src/main/java/jp/ac/mayoi/core/util/Buttons.kt
+++ phone/core/util/src/main/java/jp/ac/mayoi/core/util/Buttons.kt
@@ -3,20 +3,22 @@ package jp.ac.mayoi.core.util
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun MaigoButton(
+    modifier: Modifier = Modifier,
     onClick: () -> Unit,
     colors: ButtonColors = ButtonDefaults.buttonColors(), //後からアプリ側で設定したやつに置き換える
-    modifier: Modifier = Modifier,
     enabled: Boolean = true,
     content: @Composable RowScope.() -> Unit,
 ) {
@@ -25,8 +27,14 @@ fun MaigoButton(
         colors = colors,
         modifier = modifier
             .padding(horizontal = 16.dp, vertical = 16.dp)
-            .defaultMinSize(minWidth = 120.dp, minHeight = 60.dp),
+            .defaultMinSize(minWidth = 120.dp, minHeight = 60.dp)
+            .shadow(
+                elevation = 8.dp,
+                shape = RoundedCornerShape(40.dp),
+            ),
         enabled = enabled,
+        shape = RoundedCornerShape(40.dp),
+        elevation = ButtonDefaults.elevatedButtonElevation(defaultElevation = 16.dp), // elevationを強調
         content = content,
     )
 }
