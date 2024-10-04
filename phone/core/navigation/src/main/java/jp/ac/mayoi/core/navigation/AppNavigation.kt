18fe21bb0caeab41ec62128509825618183da7d8
koae0522 <yantukeuma@gmail.com>
2024-10-04 12:50:38 +0900

    @Serializable objectの命名を○○Navigationに変更, NavHostをPhone/appに移動

--- phone/core/navigation/src/main/java/jp/ac/mayoi/core/navigation/AppNavigation.kt
+++ phone/core/navigation/src/main/java/jp/ac/mayoi/core/navigation/AppNavigation.kt
@@ -1,54 +1,23 @@
 package jp.ac.mayoi.core.navigation
 
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import kotlinx.serialization.Serializable
 
 //インプレッションを見る
 @Serializable
-object Impressions
+object ImpressionsNavigation
 
 //オンボーディング
 @Serializable
-object Onboarding
+object OnboardingNavigation
 
 //ランキング
 @Serializable
-object Ranking
+object RankingNavigation
 
 //シェア
 @Serializable
-object Share
+object ShareNavigation
 
 //歩いてる時
 @Serializable
-object Traveling
-
-@Composable
-fun PhoneNavHost(
-    navController: NavHostController = rememberNavController(),
-){
-    NavHost(
-        navController = navController,
-        startDestination = Onboarding
-    ){
-        composable<Onboarding>{
-
-        }
-        composable<Ranking>{
-
-        }
-        composable<Impressions>{
-
-        }
-        composable<Share>{
-
-        }
-        composable<Traveling>{
-
-        }
-    }
-}
+object TravelingNavigation
