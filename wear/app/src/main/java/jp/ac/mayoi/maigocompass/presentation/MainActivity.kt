e461d1aa2ea331b7313141f566a51d675462bb67
Yourein <rein.you.3.1@gmail.com>
2024-10-04 16:18:42 +0900

    wear側のStringResourceの参照先をStringRに移行

--- wear/app/src/main/java/jp/ac/mayoi/maigocompass/presentation/MainActivity.kt
+++ wear/app/src/main/java/jp/ac/mayoi/maigocompass/presentation/MainActivity.kt
@@ -9,7 +9,6 @@ package jp.ac.mayoi.maigocompass.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -20,12 +19,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
 import androidx.wear.tooling.preview.devices.WearDevices
-import com.example.maigocompass.R
 import jp.ac.mayoi.maigocompass.presentation.theme.MaigoCompassTheme
+import jp.ac.mayoi.wear.core.resource.StringR
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,7 +62,7 @@ fun Greeting(greetingName: String) {
         modifier = Modifier.fillMaxWidth(),
         textAlign = TextAlign.Center,
         color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.hello_world, greetingName)
+        text = stringResource(StringR.hello_world, greetingName)
     )
 }
 
