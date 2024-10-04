feaf06a3be23fdd0a753c08614c583f83deadb69
ryoka-1104 <b1022160@fun.ac.jp>
2024-10-04 15:03:12 +0900

    コードの修正

--- wear/features/waiting/src/main/java/jp/ac/mayoi/wear/features/waiting/WaitingScreen.kt
+++ wear/features/waiting/src/main/java/jp/ac/mayoi/wear/features/waiting/WaitingScreen.kt
@@ -1,10 +1,11 @@
 package jp.ac.mayoi.wear.features.waiting
 
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import androidx.wear.tooling.preview.devices.WearDevices
@@ -12,15 +13,16 @@ import androidx.wear.tooling.preview.devices.WearDevices
 
 @Composable
 fun WaitingScreen() {
-    Text(
-        modifier = Modifier.padding(
-            start = 65.dp,
-            top = 86.dp,
-        ),
-        text = "ようこそ",
-
-        color = MaterialTheme.colors.primary,
-    )
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Text(
+            text = "ようこそ",
+            color = MaterialTheme.colors.primary,
+        )
+    }
 }
 
 @Preview(device = WearDevices.SMALL_ROUND, showSystemUi = true)
