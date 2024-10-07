7801823e22a1532a00be37e92b827f080cd01a2a
Yourein <rein.you.3.1@gmail.com>
2024-10-07 18:56:37 +0900

    Phone側のMaigoCompassThemeをcore:resource配下に移動

--- phone/core/resource/src/main/java/jp/ac/mayoi/core/resource/Colors.kt
+++ phone/core/resource/src/main/java/jp/ac/mayoi/core/resource/Colors.kt
@@ -1,6 +1,7 @@
 package jp.ac.mayoi.core.resource
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import jp.ac.mayoi.common.resource.buttonColorsDefaultCommon
 import jp.ac.mayoi.common.resource.colorAccentCommon
 import jp.ac.mayoi.common.resource.colorAccentSecondaryCommon
@@ -8,6 +9,14 @@ import jp.ac.mayoi.common.resource.colorBackgroundPrimaryLightCommon
 import jp.ac.mayoi.common.resource.colorBackgroundSecondaryLightCommon
 import jp.ac.mayoi.common.resource.colorTextCaptionCommon
 import jp.ac.mayoi.common.resource.colorTextMainLightCommon
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
 
 val colorBackgroundPrimary = colorBackgroundPrimaryLightCommon
 val colorBackgroundSecondary = colorBackgroundSecondaryLightCommon
