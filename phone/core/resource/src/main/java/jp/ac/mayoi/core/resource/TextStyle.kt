6e5f1ec356a197579daa65fb5ce6970feb3d16ae
Yourein <rein.you.3.1@gmail.com>
2024-10-07 18:56:37 +0900

    Phone側のMaigoCompassThemeをcore:resource配下に移動

--- phone/core/resource/src/main/java/jp/ac/mayoi/core/resource/TextStyle.kt
+++ phone/core/resource/src/main/java/jp/ac/mayoi/core/resource/TextStyle.kt
@@ -1,5 +1,10 @@
 package jp.ac.mayoi.core.resource
 
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import jp.ac.mayoi.common.resource.textStyleBodyCommon
 import jp.ac.mayoi.common.resource.textStyleCaptionCommon
 import jp.ac.mayoi.common.resource.textStyleTitleCommon
@@ -7,3 +12,29 @@ import jp.ac.mayoi.common.resource.textStyleTitleCommon
 val textStyleTitle = textStyleTitleCommon
 val textStyleBody = textStyleBodyCommon
 val textStyleCaption = textStyleCaptionCommon
+
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    )
+    /* Other default text styles to override
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+    */
+)
