e89264576266b9d82f5692de1ed4ff98c7685dec
Yourein <rein.you.3.1@gmail.com>
2024-10-07 19:05:08 +0900

    phone:appのMaigoCompassThemeをcore:resource配下のものに移行

--- phone/app/src/main/java/jp/ac/mayoi/maigocompass/MainActivity.kt
+++ phone/app/src/main/java/jp/ac/mayoi/maigocompass/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import jp.ac.mayoi.maigocompass.ui.theme.MaigoCompassTheme
+import jp.ac.mayoi.core.resource.MaigoCompassTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
