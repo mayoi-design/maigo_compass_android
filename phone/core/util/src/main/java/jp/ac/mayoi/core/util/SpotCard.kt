d3176a8cb9b53b7db978e3a0abed28c969845a09
Yourein <rein.you.3.1@gmail.com>
2024-10-10 17:50:48 +0900

    SpotCardにfootprintのiconを表示するように変更

--- phone/core/util/src/main/java/jp/ac/mayoi/core/util/SpotCard.kt
+++ phone/core/util/src/main/java/jp/ac/mayoi/core/util/SpotCard.kt
@@ -1,5 +1,6 @@
 package jp.ac.mayoi.core.util
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,11 +18,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import jp.ac.mayoi.core.resource.DrawableR
 import jp.ac.mayoi.core.resource.MaigoCompassTheme
 import jp.ac.mayoi.core.resource.colorBackgroundPrimary
 import jp.ac.mayoi.core.resource.colorBackgroundSecondary
@@ -113,8 +116,15 @@ fun SpotCard(
 private fun ReachCounter(
     count: Int,
 ) {
-    Row {
-        // Image() // ここにfootprintのアイコンをいれる
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Image(
+            painter = painterResource(DrawableR.footprint_24dp),
+            contentDescription = null,
+            modifier = Modifier
+                .size(24.dp)
+        )
         Text(
             text = "$count",
             fontSize = 12.sp,
