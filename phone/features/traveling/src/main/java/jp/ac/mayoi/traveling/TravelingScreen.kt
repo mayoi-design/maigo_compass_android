e9a4d12a21ddbfe65e5340d172aac6d42f7b0bcb
“mahochi” <523.maho@gmail.com>
2024-10-11 13:32:01 +0900

    コメントアウトの削除、必要のないModifierの削除、関数名の変更

--- phone/features/traveling/src/main/java/jp/ac/mayoi/traveling/TravelingScreen.kt
+++ phone/features/traveling/src/main/java/jp/ac/mayoi/traveling/TravelingScreen.kt
@@ -1,15 +1,13 @@
 package jp.ac.mayoi.traveling
-
-//import androidx.compose.foundation.layout.FlowColumnScopeInstance.align
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -20,45 +18,38 @@ import jp.ac.mayoi.core.resource.MaigoCompassTheme
 import jp.ac.mayoi.core.util.MaigoButton
 
 @Composable
-fun Trip(modifier: Modifier = Modifier) {
+internal fun SpotEmptyScreen(onSubmitSpot: () -> Unit) {
     Column(
         verticalArrangement = Arrangement.Center,
-        modifier = modifier
-            .width(450.dp)
-            .height(753.dp)
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
     ) {
         Text(
             fontWeight = FontWeight.Bold,
             textAlign = TextAlign.Center,
             text = "この近くにはまだスポットがないようです",
             fontSize = 16.sp,
-            lineHeight = 24.sp,
-            modifier = Modifier.fillMaxWidth()
+            lineHeight = 24.sp
         )
         Spacer(modifier = Modifier.size(24.dp))
         Text(
             fontWeight = FontWeight.Bold,
             text = "あなたが最初のスポットを\n登録してみませんか？",
-            fontSize = 16.sp,
-            lineHeight = 24.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .align(alignment = androidx.compose.ui.Alignment.CenterHorizontally)
+            fontSize = 16.sp
         )
         Spacer(modifier = Modifier.size(24.dp))
         MaigoButton(
             onClick = { },
             modifier = Modifier
-                .width(240.dp)
-                .align(alignment = androidx.compose.ui.Alignment.CenterHorizontally),
-            content = {
-                Text(
-                    fontWeight = FontWeight.Bold,
-                    text = "スポットを登録",
-                    textAlign = TextAlign.Center,
-                )
-            }
-        )
+                .width(240.dp),
+        ) {
+            Text(
+                fontWeight = FontWeight.Bold,
+                text = "スポットを登録",
+                fontSize = 16.sp
+            )
+        }
     }
 }
 
@@ -66,7 +57,8 @@ fun Trip(modifier: Modifier = Modifier) {
 @Composable
 fun TripPreview() {
     MaigoCompassTheme {
-        Trip(
+        SpotEmptyScreen(
+            onSubmitSpot = { }
         )
     }
 }
