86ada26efed76df96bf4d59c61aaed21c42d20c5
Yourein <rein.you.3.1@gmail.com>
2024-10-12 13:44:06 +0900

    koinのinit処理をBaseApplicationに実装

--- phone/core/application/src/main/java/jp/ac/mayoi/core/application/BaseApplication.kt
+++ phone/core/application/src/main/java/jp/ac/mayoi/core/application/BaseApplication.kt
@@ -1,7 +1,44 @@
 package jp.ac.mayoi.core.application
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidFileProperties
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
 
 class BaseApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
 
+        koinStarter()
+    }
+
+    private fun koinStarter() {
+        startKoin {
+            androidLogger()
+            androidContext(this@BaseApplication)
+            androidFileProperties()
+
+            modules(
+                listOf(
+                    serviceKoinModule,
+                    repositoryKoinModule,
+                    viewModelKoinModule,
+                )
+            )
+        }
+    }
+
+    private val serviceKoinModule = module {
+
+    }
+
+    private val repositoryKoinModule = module {
+
+    }
+
+    private val viewModelKoinModule = module {
+
+    }
 }
