d74b4ce4430565c4330b712e4353ccf0dbbe7d77
Yourein <rein.you.3.1@gmail.com>
2024-10-15 00:43:38 +0900

    retrofitを依存関係に追加

--- phone/service/interfaces/build.gradle.kts
+++ phone/service/interfaces/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.retrofit)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
