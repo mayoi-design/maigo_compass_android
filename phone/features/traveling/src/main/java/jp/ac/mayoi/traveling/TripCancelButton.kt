570284f1ca9681f2347f0be9a03feded3344dd7a
“mahochi” <523.maho@gmail.com>
2024-10-18 16:26:56 +0900

    旅行中断ボタンの作成

---
+++ phone/features/traveling/src/main/java/jp/ac/mayoi/traveling/TripCancelButton.kt
@@ -0,0 +1,42 @@
+package jp.ac.mayoi.traveling
+
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import jp.ac.mayoi.core.resource.MaigoCompassTheme
+import jp.ac.mayoi.core.util.MaigoButton
+
+@Composable
+fun TripCancelButton(
+    onTripCancelButtonClick: () -> Unit
+) {
+    MaigoButton(
+        onClick = onTripCancelButtonClick,
+        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFB3261E)),
+        modifier = Modifier
+            .width(200.dp),
+    ) {
+        Text(
+            fontWeight = FontWeight.Bold,
+            text = "旅行を中断",
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TripCancelPreview() {
+    MaigoCompassTheme {
+        TripCancelButton(
+            onTripCancelButtonClick = { }
+        )
+    }
+}
