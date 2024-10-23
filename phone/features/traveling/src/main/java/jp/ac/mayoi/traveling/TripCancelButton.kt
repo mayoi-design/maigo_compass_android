b1eb8ea0f86c49dba6603af3e52777717acf8d71
“mahochi” <523.maho@gmail.com>
2024-10-23 15:30:17 +0900

    stringResourceを使ってボタンの文字の表示

--- phone/features/traveling/src/main/java/jp/ac/mayoi/traveling/TripCancelButton.kt
+++ phone/features/traveling/src/main/java/jp/ac/mayoi/traveling/TripCancelButton.kt
@@ -6,11 +6,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import jp.ac.mayoi.core.resource.MaigoCompassTheme
+import jp.ac.mayoi.core.resource.StringR
 import jp.ac.mayoi.core.util.MaigoButton
 
 @Composable
@@ -25,7 +27,7 @@ fun TripCancelButton(
     ) {
         Text(
             fontWeight = FontWeight.Bold,
-            text = "旅行を中断",
+            text = stringResource(StringR.traveling_cancel_travel),
             fontSize = 16.sp
         )
     }
