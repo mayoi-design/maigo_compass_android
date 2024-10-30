8efc78bc480310c2c20c606da317d4b4e8311af9
iuyoKatano <yosiuyk@yahoo.co.jp>
2024-10-30 15:37:03 +0900

    不要インポート削除

--- phone/features/ranking/src/main/java/jp/ac/mayoi/ranking/AreaSelectButton.kt
+++ phone/features/ranking/src/main/java/jp/ac/mayoi/ranking/AreaSelectButton.kt
@@ -1,11 +1,9 @@
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import jp.ac.mayoi.phone.model.RemoteRankingArea
@@ -35,6 +33,28 @@ internal fun AreaSelectButtons(
         }
     }
 }
+
+@Preview
+@Composable
+private fun RankingScreen() {
+    val areas = listOf(
+        RemoteRankingArea("1", "エリアA"),
+        RemoteRankingArea("2", "エリアB"),
+        RemoteRankingArea("3", "名前がちょっと長いエリア")
+    ).toImmutableList()
+    var selectedArea by remember { mutableStateOf<RemoteRankingArea?>(null) }
+
+    Column {
+        AreaSelectButtons(
+            areas = areas,
+            currentSelection = selectedArea,
+            onAreaSelectButtonClick = { selectedArea = it }
+        )
+
+        Text(text = "選択されたエリア: ${selectedArea?.areaName ?: "未選択"}") // ?.name でアクセス
+    }
+}
+
 
 
 
