8983437d1a365fd50850197007549e355e350dc0
“mahochi” <523.maho@gmail.com>
2024-10-30 17:59:34 +0900

    spacerの値をspacingTripleに変更、Text Composableを呼んだと気にtextを一番上で渡すよう順番を変更、style = textStyleBodyに変更

--- phone/features/traveling/src/main/java/jp/ac/mayoi/traveling/TravelingErrorScreen.kt
+++ phone/features/traveling/src/main/java/jp/ac/mayoi/traveling/TravelingErrorScreen.kt
@@ -12,13 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import jp.ac.mayoi.core.resource.MaigoCompassTheme
 import jp.ac.mayoi.core.resource.StringR
+import jp.ac.mayoi.core.resource.spacingTriple
+import jp.ac.mayoi.core.resource.textStyleBody
 import jp.ac.mayoi.core.util.MaigoButton
 
 @Composable
@@ -35,29 +35,24 @@ internal fun TravelingErrorScreen(
                 .fillMaxSize()
         ) {
             Text(
-                fontWeight = FontWeight.Bold,
                 text = stringResource(StringR.traveling_connection_failed),
-                fontSize = 16.sp,
-                lineHeight = 24.sp
+                style = textStyleBody
             )
-            Spacer(modifier = Modifier.size(24.dp))
+            Spacer(modifier = Modifier.size(spacingTriple))
             Text(
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center,
                 text = stringResource(StringR.traveling_retry_section),
-                fontSize = 16.sp,
-                lineHeight = 24.sp
+                style = textStyleBody,
+                textAlign = TextAlign.Center
             )
-            Spacer(modifier = Modifier.size(24.dp))
+            Spacer(modifier = Modifier.size(spacingTriple))
             MaigoButton(
                 onClick = onRetryButtonClick,
                 modifier = Modifier
                     .width(240.dp),
             ) {
                 Text(
-                    fontWeight = FontWeight.Bold,
                     text = stringResource(StringR.general_retry),
-                    fontSize = 16.sp
+                    style = textStyleBody
                 )
             }
         }
