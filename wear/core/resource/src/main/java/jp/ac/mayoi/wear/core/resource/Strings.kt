975e096fb9f5b7ec266ab1ac45a843dd8f74ecc6
konno yusei <bakery_panya@yahoo.co.jp>
2024-10-30 15:16:02 +0900

    LocationServiceファイルを追加、それに伴った設定追加

--- wear/core/resource/src/main/java/jp/ac/mayoi/wear/core/resource/Strings.kt
+++ wear/core/resource/src/main/java/jp/ac/mayoi/wear/core/resource/Strings.kt
@@ -1,2 +1,5 @@
 package jp.ac.mayoi.wear.core.resource
 
+const val locationIntentAction = "MAIGO_COMPASS_LOCATION_BROADCAST"
+const val locationIntentLatitude = "Latitude"
+const val locationIntentLongitude = "Longitude"
