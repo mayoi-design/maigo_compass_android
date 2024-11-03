2006bba01fc4c7493d3f43b1018a4f9ced2b951c
Yourein <rein.you.3.1@gmail.com>
2024-11-03 18:16:24 +0900

    DevelopmentTravelingRepositoryを実装

--- phone/app/src/development/java/jp/ac/mayoi/maigocompass/mock/DevelopmentTravelingRepository.kt
+++ phone/app/src/development/java/jp/ac/mayoi/maigocompass/mock/DevelopmentTravelingRepository.kt
@@ -2,9 +2,22 @@ package jp.ac.mayoi.maigocompass.mock
 
 import jp.ac.mayoi.phone.model.RemoteSpot
 import jp.ac.mayoi.repository.interfaces.TravelingRepository
+import kotlinx.coroutines.delay
 
 class DevelopmentTravelingRepository : TravelingRepository {
     override suspend fun getNearSpots(lat: Double, lng: Double): List<RemoteSpot> {
-        TODO("Not yet implemented")
+        delay(2000)
+        return List(10) {
+            RemoteSpot(
+                lat = 0.0f,
+                lng = 0.0f,
+                message = "近くのスポット ${it + 1}",
+                imageUrl = "",
+                postUserId = "",
+                createdAt = "2024-11-03T00:00:00+09:00",
+                reachedCount = 10 * it,
+                spotId = "",
+            )
+        }
     }
 }
