3dcb631d072bfe6bd7a65a2f8fc1f456872f4074
Yourein <rein.you.3.1@gmail.com>
2024-11-03 18:29:54 +0900

    UserMemoryの定義が間違っていたので更新

--- phone/model/src/main/java/jp/ac/mayoi/phone/model/UserMemory.kt
+++ phone/model/src/main/java/jp/ac/mayoi/phone/model/UserMemory.kt
@@ -3,7 +3,7 @@ package jp.ac.mayoi.phone.model
 data class UserMemory(
     val totalTripCount: Int,
     val spotShareCount: Int,
-    val areaShareCounts: List<UserMemory>,
+    val areaShareCounts: List<MemoryAreaModel>,
     val exploreSpotCount: Int,
     val firstTripDate: String?,
     val latestTripDate: String?,
