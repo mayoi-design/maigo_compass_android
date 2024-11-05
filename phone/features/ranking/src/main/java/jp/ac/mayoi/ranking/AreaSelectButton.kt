4697c642aaa96a66ee47d97897cf100109e0f491
iuyoKatano <yosiuyk@yahoo.co.jp>
2024-11-06 00:57:34 +0900

    プレビュー以外の修正点反映

--- phone/features/ranking/src/main/java/jp/ac/mayoi/ranking/AreaSelectButton.kt
+++ phone/features/ranking/src/main/java/jp/ac/mayoi/ranking/AreaSelectButton.kt
@@ -1,16 +1,23 @@
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import jp.ac.mayoi.core.resource.spacingDouble
+import jp.ac.mayoi.core.resource.spacingSingle
 import jp.ac.mayoi.phone.model.RemoteRankingArea
 import jp.ac.mayoi.ranking.RankingButtonChip
 import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.toImmutableList
-
+import kotlinx.collections.immutable.persistentListOf
 
 @Composable
 internal fun AreaSelectButtons(
@@ -21,8 +28,8 @@ internal fun AreaSelectButtons(
     LazyRow(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 16.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
+            .padding(horizontal = spacingDouble),
+        horizontalArrangement = Arrangement.spacedBy(spacingSingle)
     ) {
         items(areas) { area ->
             RankingButtonChip(
@@ -34,18 +41,19 @@ internal fun AreaSelectButtons(
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun RankingScreen() {
-    val areas = listOf(
-        RemoteRankingArea("エリア名", "1"),
-        RemoteRankingArea("エリア名", "2"),
-        RemoteRankingArea("名前がちょっと長いエリア", "3")
-    ).toImmutableList()
+    val areas = persistentListOf(
+        RemoteRankingArea("エリア名1", "1"),
+        RemoteRankingArea("エリア名2", "2"),
+        RemoteRankingArea("名前がちょっと長いエリア名", "3")
+    )
     var selectedArea by remember { mutableStateOf<RemoteRankingArea?>(null) }
 
-    Column {
-
+    Column(
+        modifier = Modifier.padding(spacingDouble)
+    ) {
         if (areas.isEmpty()) {
             Text("エリアがありません")
         } else {
@@ -59,5 +67,3 @@ private fun RankingScreen() {
         }
     }
 }
-
-
