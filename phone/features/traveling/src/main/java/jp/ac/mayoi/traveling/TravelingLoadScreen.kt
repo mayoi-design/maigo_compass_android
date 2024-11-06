18f47377a0ca678feb7a359258fa1e50dd64ecd5
“mahochi” <523.maho@gmail.com>
2024-11-06 15:10:36 +0900

    internalの追加、中央寄せにColumnを使う必要がなかったので削除し、modifierでalignをつけた

--- phone/features/traveling/src/main/java/jp/ac/mayoi/traveling/TravelingLoadScreen.kt
+++ phone/features/traveling/src/main/java/jp/ac/mayoi/traveling/TravelingLoadScreen.kt
@@ -1,8 +1,6 @@
 package jp.ac.mayoi.traveling
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
@@ -12,19 +10,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import jp.ac.mayoi.core.resource.MaigoCompassTheme
 
 @Composable
-fun TravelingLoadScreen(
+internal fun TravelingLoadScreen(
     onTripCancelButtonClick: () -> Unit
 ) {
     Box(modifier = Modifier.fillMaxSize()) {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
+        CircularProgressIndicator(
             modifier = Modifier
-                .fillMaxSize()
-        ) {
-            CircularProgressIndicator(
-            )
-        }
+                .align(Alignment.Center)
+        )
         Box(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
