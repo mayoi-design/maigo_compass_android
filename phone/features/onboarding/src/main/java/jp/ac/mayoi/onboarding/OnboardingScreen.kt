1136d828d9e503db6f99b628328d6a778f95a744
koae0522 <yantukeuma@gmail.com>
2024-11-08 17:31:28 +0900

    modifierを引数の最後に移動、 ボタンの文章とアイコンをphone:core:resに移動

--- phone/features/onboarding/src/main/java/jp/ac/mayoi/onboarding/OnboardingScreen.kt
+++ phone/features/onboarding/src/main/java/jp/ac/mayoi/onboarding/OnboardingScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.model.CameraPosition
@@ -23,7 +24,9 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.rememberCameraPositionState
+import jp.ac.mayoi.core.resource.DrawableR
 import jp.ac.mayoi.core.resource.MaigoCompassTheme
+import jp.ac.mayoi.core.resource.StringR
 import jp.ac.mayoi.core.resource.colorAccentSecondary
 import jp.ac.mayoi.core.resource.spacingDouble
 import jp.ac.mayoi.core.resource.spacingSingle
@@ -40,31 +43,31 @@ fun OnboardingScreen(
     }
     Box() {
         GoogleMap(
-            modifier = Modifier.fillMaxSize(),
             uiSettings = MapUiSettings(
                 zoomControlsEnabled = false,
             ),
             cameraPositionState = cameraPositionState,
+            modifier = Modifier.fillMaxSize()
         )
         Column(
+            horizontalAlignment = Alignment.End,
             modifier = Modifier
                 .fillMaxWidth()
                 .align(Alignment.BottomCenter)
-                .padding(vertical = spacingSingle),
-            horizontalAlignment = Alignment.End,
+                .padding(vertical = spacingSingle)
         ) {
             FloatingActionButton(
                 onClick = { onCurrentPositionClicked() },
-                modifier = Modifier
-                    .padding(horizontal = spacingDouble),
                 shape = CircleShape,
                 containerColor = Color.White,
                 elevation = FloatingActionButtonDefaults.elevation(
                     defaultElevation = 4.dp,
-                )
+                ),
+                modifier = Modifier
+                    .padding(horizontal = spacingDouble)
             ) {
                 Icon(
-                    painter = painterResource(R.drawable.icon_current_position),
+                    painter = painterResource(DrawableR.icon_current_position),
                     contentDescription = null,
                     tint = colorAccentSecondary,
                     modifier = Modifier.size(32.dp),
@@ -73,7 +76,7 @@ fun OnboardingScreen(
             MaigoButton(
                 onClick = onDecideClicked,
                 content = {
-                    Text("目的地を決定")
+                    Text(text = stringResource(StringR.onboarding_decide))
                 },
                 modifier = Modifier.fillMaxWidth(),
             )
