d1bd1c679493fe07842a12249841f8d859502dfe
Yourein <rein.you.3.1@gmail.com>
2024-11-13 21:15:38 +0900

    postUserIdの消し忘れを直した

--- phone/app/src/development/java/jp/ac/mayoi/maigocompass/mock/DevelopmentTravelingRepository.kt
+++ phone/app/src/development/java/jp/ac/mayoi/maigocompass/mock/DevelopmentTravelingRepository.kt
@@ -22,7 +22,6 @@ class DevelopmentTravelingRepository : TravelingRepository {
                 lng = 0.0f,
                 message = "近くのスポット ${it + 1}",
                 imageUrl = "",
-                postUserId = "",
                 createdAt = "2024-11-03T00:00:00+09:00",
                 reachedCount = 10 * it,
                 spotId = "",
