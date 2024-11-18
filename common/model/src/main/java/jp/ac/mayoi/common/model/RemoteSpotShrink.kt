a55183fdad9eb1dc65fbbd41da54bdcaf1482f04
Yourein <rein.you.3.1@gmail.com>
2024-11-19 00:32:20 +0900

    RemoteSpotShrinkからLocationクラスを取れるようにした

--- common/model/src/main/java/jp/ac/mayoi/common/model/RemoteSpotShrink.kt
+++ common/model/src/main/java/jp/ac/mayoi/common/model/RemoteSpotShrink.kt
@@ -1,8 +1,16 @@
 package jp.ac.mayoi.common.model
 
+import android.location.Location
+
 data class RemoteSpotShrink(
     val lat: Double,
     val lng: Double,
     val comment: String,
     // val emoji: なんか
-)
+) {
+    val location: Location
+        get() = Location(null).also {
+            it.latitude = this.lat
+            it.longitude = this.lng
+        }
+}
