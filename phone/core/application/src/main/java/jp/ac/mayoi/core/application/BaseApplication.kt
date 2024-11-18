7cb44a5e9223b35aa87c2f3c01495e02e949a483
iuyoKatano <yosiuyk@yahoo.co.jp>
2024-11-18 17:57:15 +0900

    Koinへの登録を色々調べてやってみた(合ってるかはわからん)　一部コメント追加

--- phone/core/application/src/main/java/jp/ac/mayoi/core/application/BaseApplication.kt
+++ phone/core/application/src/main/java/jp/ac/mayoi/core/application/BaseApplication.kt
@@ -81,7 +81,7 @@ abstract class BaseApplication : Application() {
         single {
             // UAなどの設定も後からBuilderに追加する
             Retrofit.Builder()
-                .baseUrl(BuildConfig.MAIGO_COMPASS_API_URL)
+                .baseUrl(BuildConfig.MAIGO_COMPASS_API_URL) //ここ、特に弄っては無いのですが、怒られてたので見て欲しいです
                 .addConverterFactory(
                     Json.asConverterFactory(
                         "application/json".toMediaType()
@@ -103,7 +103,9 @@ abstract class BaseApplication : Application() {
     }
 
     private val repositoryKoinModule = module {
-
+        single<RankingRepository> {
+            RankingRepositoryImpl(get()) //調べた感じ、今回の場合factoryよりsingleの方が良さそうな気がしたんですけどどうなんでしょう？
+        }
     }
 
     private val viewModelKoinModule = module {
---
+++ phone/repository/implementations/src/main/java/jp/ac/mayoi/repository/implementations/RankingRepositoryImpl.kt
@@ -0,0 +1,22 @@
+package jp.ac.mayoi.repository.implementations
+
+import jp.ac.mayoi.phone.model.RemoteRankingArea
+import jp.ac.mayoi.phone.model.RemoteSpot
+import jp.ac.mayoi.repository.interfaces.RankingRepository
+import jp.ac.mayoi.service.interfaces.RankingService
+
+class RankingRepositoryImpl(
+    private val rankingService: RankingService,
+) : RankingRepository {
+
+    override suspend fun getAvailArea(): List<RemoteRankingArea> {
+        return rankingService.getRankingArea()
+    }
+
+    override suspend fun getRanking(areaId: String): List<RemoteSpot> {
+        val areaIdInt = areaId.toIntOrNull()
+            ?: throw IllegalArgumentException("Invalid areaId: $areaId")
+
+        return rankingService.getRanking(areaIdInt)
+    }
+}
