14162bb416ba2350884d28ae0e61f4b1426ac4ad
Yourein <rein.you.3.1@gmail.com>
2024-11-24 07:24:03 +0900

    テストケースを修正

--- wear/repository/implementations/src/test/kotlin/jp/ac/mayoi/wear/repository/implementations/LocationRepositoryImplTest.kt
+++ wear/repository/implementations/src/test/kotlin/jp/ac/mayoi/wear/repository/implementations/LocationRepositoryImplTest.kt
@@ -5,6 +5,13 @@ import org.junit.Test
 
 @Suppress("NonAsciiCharacters", "TestFunctionName")
 class LocationRepositoryImplTest {
+    // テストケースの作り方
+    // 1. 現在地と目的地と向いている方位角を決める
+    // 2. 方位角を方位系から数学系に直す
+    // 3. (x, y) = (1, 0)を回転行列を用いて2で求めた数学系の角度だけ回す
+    // 3.1. 新しい点の位置を ax, ay とする
+    // 4. 目的地に向かうベクトルを計算し、その座標をbx, byとする
+    // 5. θ = acos((ax*bx + ay*by) / (sqrt(ax**2 + ay**2)*sqrt(bx**2 + by**2))) を計算する
 
     @Test
     fun `365から函館駅へ`() {
@@ -17,9 +24,8 @@ class LocationRepositoryImplTest {
         val destinationLng = 140.72677811248317
         val currentHeading = 0.0
 
-        // https://www.wolframalpha.com/input?i2d=true&i=atan2%5C%2840%2941.773674691098705+-+41.842116783883995%5C%2844%29+140.72677811248317+-+140.76710646276334%5C%2841%29+*+Divide%5B180%2CPI%5D+-+90+mod+360
         // 365室前から真北を向いた時、函館駅は南南西方面にある
-        val expected = 149.49199786282
+        val expected = 149.49199786282134
         val actual = LocationRepositoryImpl()
             .getBearing(
                 currentLat = currentLat,
@@ -44,7 +50,6 @@ class LocationRepositoryImplTest {
 
         val currentHeading = 0.0
 
-        // https://www.wolframalpha.com/input?i2d=true&i=atan2%5C%2840%2941.781998336369114+-+41.773674691098705%5C%2844%29+140.79100220973095+-+140.72677811248317%5C%2841%29+*+Divide%5B180%2CPI%5D+-+90+mod+360
         // 函館駅前から真北を向いた時、湯倉神社は東北東方面にある
         val expected = 277.38455101414
         val actual = LocationRepositoryImpl()
@@ -69,12 +74,11 @@ class LocationRepositoryImplTest {
         val destinationLat = 41.77596538110248
         val destinationLng = 140.81591755754837
 
-        // 昭和ターミナル方面を向く ()
+        // 昭和ターミナル方面を向く (数学系では120°)
         val currentHeading = 330.0
 
-        // https://www.wolframalpha.com/input?i2d=true&i=atan2%5C%2840%2941.781998336369114+-+41.773674691098705%5C%2844%29+140.79100220973095+-+140.72677811248317%5C%2841%29+*+Divide%5B180%2CPI%5D+-+90+mod+360
         // 亀田支所前から昭和ターミナル方面を向いた時、函館空港はほぼ真後ろにあるはず
-        val expected = 277.38455101414
+        val expected = 360.0 - 151.6443253181512
         val actual = LocationRepositoryImpl()
             .getBearing(
                 currentLat = currentLat,
