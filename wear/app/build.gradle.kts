b562f84a69990208715dbcf6544133355d2ce1a4
konno yusei <bakery_panya@yahoo.co.jp>
2024-11-23 22:37:43 +0900

    applicationIdを変更

--- wear/app/build.gradle.kts
+++ wear/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.maigocompass"
+        applicationId = "jp.ac.mayoi.maigocompass"
         minSdk = 30
         targetSdk = 35
         versionCode = 1
