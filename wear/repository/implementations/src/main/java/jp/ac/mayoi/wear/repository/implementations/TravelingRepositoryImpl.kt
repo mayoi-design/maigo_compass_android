638c2fb45a79c3a20a7ae616970facdbdf692a97
Yourein <rein.you.3.1@gmail.com>
2024-12-09 22:43:09 +0900

    一旦TravelingRepositoryがWearableListenerServiceを継承するのをやめた

--- wear/repository/implementations/src/main/java/jp/ac/mayoi/wear/repository/implementations/TravelingRepositoryImpl.kt
+++ wear/repository/implementations/src/main/java/jp/ac/mayoi/wear/repository/implementations/TravelingRepositoryImpl.kt
@@ -1,51 +1,15 @@
 package jp.ac.mayoi.wear.repository.implementations
 
-import android.content.Intent
-import android.util.Log
-import com.google.android.gms.wearable.MessageEvent
-import com.google.android.gms.wearable.WearableListenerService
 import jp.ac.mayoi.common.model.RemoteSpotShrinkList
 import jp.ac.mayoi.wear.repository.interfaces.TravelingRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
-import kotlinx.serialization.json.Json
 
 class TravelingRepositoryImpl(
     initialValue: RemoteSpotShrinkList = RemoteSpotShrinkList(listOf())
 ) : TravelingRepository() {
-
     private val _recommendSpot: MutableStateFlow<RemoteSpotShrinkList> = MutableStateFlow(initialValue)
     override val recommendSpot: StateFlow<RemoteSpotShrinkList> = _recommendSpot
-
-    override fun onCreate() {
-        super.onCreate()
-        Log.d("TravelingRepository", "Created")
-    }
-
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.d("TravelingRepository", "Started")
-        return super.onStartCommand(intent, flags, startId)
-    }
-
-    override fun onMessageReceived(messageEvent: MessageEvent) {
-        Log.d("Message", "message receive")
-        when (messageEvent.path) {
-            LOCATION_DATA -> {
-                Log.d("Message", "message Changed")
-                //jsonをここでデシリアライズする
-                //spotにいれる
-                val deserialized = Json.decodeFromString<RemoteSpotShrinkList>(messageEvent.data.toString(Charsets.UTF_8))
-
-                CoroutineScope(Dispatchers.IO).launch {
-                    _recommendSpot.emit(deserialized)
-                }
-
-            }
-        }
-    }
 
     companion object {
         const val LOCATION_DATA = "/location-data"
--- wear/repository/interfaces/src/main/java/jp/ac/mayoi/wear/repository/interfaces/TravelingRepository.kt
+++ wear/repository/interfaces/src/main/java/jp/ac/mayoi/wear/repository/interfaces/TravelingRepository.kt
@@ -1,11 +1,8 @@
 package jp.ac.mayoi.wear.repository.interfaces
 
-import com.google.android.gms.wearable.MessageClient
-import com.google.android.gms.wearable.WearableListenerService
-import jp.ac.mayoi.common.model.RemoteSpotShrink
 import jp.ac.mayoi.common.model.RemoteSpotShrinkList
 import kotlinx.coroutines.flow.StateFlow
 
-abstract class TravelingRepository : WearableListenerService()  {
+abstract class TravelingRepository {
     abstract val recommendSpot: StateFlow<RemoteSpotShrinkList>
 }
