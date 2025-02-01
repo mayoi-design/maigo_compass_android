2b697c322e21bcfc626dcd650cf05630fa2f3449
Yourein <rein.you.3.1@gmail.com>
2025-02-01 23:48:20 +0900

    クリックしたエントリの情報をグラフの中心に表示するようにした

--- phone/features/memory/src/main/java/jp/ac/mayoi/memory/graph/MemoryGraph.kt
+++ phone/features/memory/src/main/java/jp/ac/mayoi/memory/graph/MemoryGraph.kt
@@ -1,40 +1,72 @@
 package jp.ac.mayoi.memory.graph
 
-import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.min
+import jp.ac.mayoi.core.resource.MaigoCompassTheme
+import jp.ac.mayoi.core.resource.colorGraphMihara
+import jp.ac.mayoi.core.resource.colorGraphYunokawa
+import jp.ac.mayoi.phone.model.PieChartEntry
 import jp.ac.mayoi.phone.model.PieChartModel
 
 @Composable
 fun MemoryGraph(
     model: PieChartModel,
+    selectedEntry: Int?,
+    onChartEntrySelected: (Int?) -> Unit,
 ) {
-    var selectedEntry: Int? by remember { mutableStateOf(null) }
-    val context = LocalContext.current
+    val chartBoxSize = 550.dp
     Box {
         MemoryPieChart(
             model = model,
             selectedEntry = selectedEntry,
-            onEntrySelected = {
-                Toast.makeText(
-                    context,
-                    "Entry $it has selected",
-                    Toast.LENGTH_SHORT
-                ).show()
-                selectedEntry = it
-            },
+            onEntrySelected = onChartEntrySelected,
             modifier = Modifier
                 .fillMaxWidth()
-                .height(350.dp)
+                .height(chartBoxSize)
+        )
+
+        val infoBoxSize = min(chartBoxSize / 2, 225.dp)
+        MemoryPieChartInfo(
+            model = model,
+            selectedEntry = selectedEntry,
+            modifier = Modifier
+                .size(infoBoxSize)
+                .align(Alignment.Center)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun MemoryGraphPreview() {
+    MaigoCompassTheme {
+        val model = PieChartModel(
+            listOf(
+                PieChartEntry(
+                    label = "函館駅前・大門",
+                    arcColor = colorGraphYunokawa,
+                    count = 60,
+                ),
+                PieChartEntry(
+                    label = "LabelB",
+                    arcColor = colorGraphMihara,
+                    count = 1,
+                ),
+            )
+        )
+
+        MemoryGraph(
+            model = model,
+            selectedEntry = 0,
+            onChartEntrySelected = {},
         )
     }
 }
